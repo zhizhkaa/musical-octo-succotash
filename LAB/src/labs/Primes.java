5eb9f4da6d46e1cf8678646dfbadeeab5a57dd21
zhizhkaa <70818655+zhizhkaa@users.noreply.github.com>
2021-02-18 16:36:57 +0300

    Add files via upload

---
+++ LAB/src/labs/Primes.java
@@ -0,0 +1,20 @@
+package labs;
+
+public class Primes {
+
+	public static void main(String[] args) {
+		for (int i = 2; i <= 100; i++) 
+			if (isPrime(i) == true) 
+				System.out.println(i);		
+	}
+	
+	// Проверка числа на чётность
+	
+	public static boolean isPrime(int num) {
+		for (int i = 2; i < num; i++) 
+			if (num % i == 0) 
+				return false;
+		return true;
+	}
+
+}
